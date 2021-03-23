cf79da2894e5239b3fd90b64f4fe85c5a2e63a5e
caitlinbrooks03 <64147934+caitlinbrooks03@users.noreply.github.com>
2021-03-23 12:37:36 -0500

    Created classes for each frame, and converted the function to log in to be an appropriate method of the LogInPage class.
    #note: needs to have the windows resized to an appropriate scaling

--- loginView.py
+++ loginView.py
@@ -1,52 +1,87 @@
 from tkinter import *
 
 
+class App(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        
+        container = Frame(self)
+        
+        container.pack(side="top", fill= "both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight =1)
 
-#Button Function
-#-------------------------------------------
-def LogInFunc():
+        #the frames are stored here
+        self.frames = {}
+        for F in (LogInPage, Dashboard):
+            page_name = F.__name__
+            frame = F(parent = container, controller = self)
+            self.frames[page_name] = frame
 
-    #get the string from the entry field
-    userName = usernameTF.get()
-    passWord = passwordTF.get()
+            frame.grid(row=0, column = 0, sticky = "nsew")
 
-    print("Username: " + userName + "\nPassword: " + passWord)
+        self.show_frame("LogInPage")
 
-    #clear the entry fields 
-    usernameTF.delete(0, END)
-    passwordTF.delete(0, END)
-
-    #Below: the functionality of the button should be entered here
-    #Such as the query
-    print("This will login eventually")
-
-
-#Setting up window
-#-------------------------------------------
-window = Tk()
-window.title("Login")
-window.geometry("400x400")
-
-#Code for widgits
-#-------------------------------------------
-
-#Creation of the available fields
-userLabel = Label(window, text = "Username: ")
-usernameTF = Entry(window, bd = 5)
-passwordLabel = Label(window, text = "Password: ")
-passwordTF = Entry(window, bd = 5)
-LogIn = Button(window, text = "Log In", command = LogInFunc)
+    def show_frame(self, page_name):
+        #Show a frame for the given page name
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 
-#Placing the fields in the window
-#------------------------------------------
-userLabel.grid(row=0, column=0)
-usernameTF.grid(row=0, column=1)
-passwordLabel.grid(row = 1, column = 0)
-passwordTF.grid(row = 1, column = 1)
-LogIn.grid(row = 2, column = 1)
+class LogInPage(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        
+
+        #Creation of the available fields
+        userLabel = Label(self, text = "Username: ")
+        self.usernameTF = Entry(self, bd = 5)
+        passwordLabel = Label(self, text = "Password: ")
+        self.passwordTF = Entry(self, bd = 5)
+
+        LogIn = Button(self, text = "Log In", command = self.LogInFunc)
+        
+        #Layout of the available fields
+        userLabel.grid(row=0, column=0)
+        self.usernameTF.grid(row=0, column=1)
+        passwordLabel.grid(row = 1, column = 0)
+        self.passwordTF.grid(row = 1, column = 1)
+        LogIn.grid(row = 2, column = 1)
+
+    
+        #Button Function
+    #-------------------------------------------
+    def LogInFunc(self):
+
+        #get the string from the entry field
+        userName = self.usernameTF.get()
+        passWord = self.passwordTF.get()
+
+        print("Username: " + userName + "\nPassword: " + passWord)
+
+        #Below: the functionality of the button should be entered here
+        #Such as the query
+        
+        print("This will login eventually")
+
+        #How to switch frames
+        self.controller.show_frame("Dashboard")
+        
+
+#This is the new generic frame
+class Dashboard(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+        label = Label(self, text = "This will be the view on login")
+        label.pack(side = "top", fill = "x", pady=10)
 
 
 
 if __name__ == "__main__":
-    window.mainloop()
+    app = App()
+    app.mainloop()
